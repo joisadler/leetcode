ea8ddfbf86a1b607d5ced9d2e03ae5a0d906c080
Yossi Adler <joisadler@gmail.com>
2017-10-29 04:57:01 +0200

    add two sum second solution

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -6,7 +6,7 @@
 
 
 /*
-Brute force method, accepted but inefficient (O(n)^2):
+Brute force method, accepted but inefficient, O(n)^2
 */
 
 const twoSum = (nums, target) => {
@@ -17,4 +17,30 @@ const twoSum = (nums, target) => {
             return [nums.indexOf(first), nums.lastIndexOf(second)];
         }
     }
+    throw new Error('No two sum solution');
+};
+
+/*
+Approach #2 (Two-pass Hash Table), O(n)
+*/
+
+const twoSum2 = (nums, target) => {
+    const hash = new Map();
+
+    for (let i = 0; i < nums.length; i += 1) {
+        const value = nums[i];
+        const index = i;
+        hash.set(value, index);
+    }
+    for (let i = 0; i < nums.length; i += 1) {
+        const firstNumber = nums[i];
+        const firstNumberIndex = i;
+        const complement = target - firstNumber;
+        const complementIndex = hash.get(complement);
+
+        if (hash.has(complement) && complementIndex !== firstNumberIndex) {
+            return [firstNumberIndex, complementIndex];
+        }
+    }
+    throw new Error('No two sum solution');
 };
