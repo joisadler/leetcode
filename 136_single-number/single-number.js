96ad567001065c26b569597be018911dcbcaa77a
Yossi Adler <joisadler@gmail.com>
2017-10-30 13:38:25 +0200

    add single number accepted solution

---
+++ 136_single-number/single-number.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = (nums) => {
+    const map = new Map();
+    let result = 0;
+
+    for (let i = 0; i < nums.length; i += 1) {
+        const currentNum = nums[i];
+        const counter = map.has(currentNum) ? 2 : 1;
+
+        map.set(currentNum, counter);
+    }
+    map.forEach((value, key) => {
+        if (value === 1) {
+            result = key;
+        }
+    });
+    return result;
+};
+
+export default singleNumber;
