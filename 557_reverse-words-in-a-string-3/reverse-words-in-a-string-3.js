a5735cd149fab5954282e384fbd5976bb58d93c9
Yossi Adler <joisadler@gmail.com>
2017-11-01 10:44:53 +0200

    add reverse words in a string 3 accepted solution

---
+++ 557_reverse-words-in-a-string-3/reverse-words-in-a-string-3.js
@@ -0,0 +1,13 @@
+const reverseWords = (str) => {
+    const arr = [];
+    str.split(' ').forEach((word) => {
+        let result = '';
+        for (let i = word.length - 1; i >= 0; i -= 1) {
+            result += word[i];
+        }
+        arr.push(result);
+    });
+    return arr.join(' ');
+};
+
+export default reverseWords;
