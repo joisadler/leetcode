ec40c3490f00c971f268b61d6e9966b0eef42c09
Yossi Adler <joisadler@gmail.com>
2017-11-01 11:37:58 +0200

    add two reverse string accepted solutions

---
+++ 344_reverse-string/reverse-string.js
@@ -0,0 +1,19 @@
+/*
+Short way
+*/
+
+const reverseString = s => s.split('').reverse().join('');
+
+/*
+Longer and trivial way but still more efficient
+*/
+
+const reverseString2 = (s) => {
+    let result = '';
+    for (let i = s.length - 1; i >= 0; i -= 1) {
+        result += s[i];
+    }
+    return result;
+};
+
+export { reverseString, reverseString2 };
