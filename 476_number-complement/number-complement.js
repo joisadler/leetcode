dba18fc7fb3a09c4a3e53191a44176febd6e81fd
Yossi Adler <joisadler@gmail.com>
2017-11-01 02:27:49 +0200

    add number complement accepted solution

---
+++ 476_number-complement/number-complement.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const findComplement = (num) => {
+    const bin = num.toString(2);
+    let newBin = '';
+
+    for (let i = 0; i < bin.length; i += 1) {
+        const current = bin[i];
+        newBin += String(Number(!(Number(current))));
+    }
+    const dec = parseInt(newBin, 2);
+    return dec;
+};
+
+export default findComplement;
