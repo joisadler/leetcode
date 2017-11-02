4a02c3a3bb51cd9d062eb1ae43f78a933ed7a2e8
Yossi Adler <joisadler@gmail.com>
2017-11-02 02:32:47 +0200

    add accepted Fizz Buzz solution

---
+++ 412_fizz-buzz/fizz-buzz.js
@@ -0,0 +1,35 @@
+const fizzBuzz = (n) => {
+    const result = [];
+    for (let i = 1; i <= n; i += 1) {
+        let currentStr = String(i);
+        if (i % 3 === 0 && i % 5 === 0) {
+            currentStr = 'FizzBuzz';
+        } else if (i % 3 === 0) {
+            currentStr = 'Fizz';
+        } else if (i % 5 === 0) {
+            currentStr = 'Buzz';
+        }
+        result.push(currentStr);
+    }
+    return result;
+};
+
+console.log(fizzBuzz(15));
+/* [ '1',
+  '2',
+  'Fizz',
+  '4',
+  'Buzz',
+  'Fizz',
+  '7',
+  '8',
+  'Fizz',
+  'Buzz',
+  '11',
+  'Fizz',
+  '13',
+  '14',
+  'FizzBuzz' ]
+*/
+
+export default fizzBuzz;
