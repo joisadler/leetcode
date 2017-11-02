de4b02da8fc069528161f318bf7bfd6437e407d2
Yossi Adler <joisadler@gmail.com>
2017-11-02 10:38:54 +0200

    add "reshape the matrix" solution

---
+++ 566_reshape-the-matrix/reshape-the-matrix.js
@@ -0,0 +1,49 @@
+const matrixReshape = (nums, r, c) => {
+    const originalR = nums.length;
+    const originalC = nums[0].length;
+    const result = [];
+
+    if (r * c > originalR * originalC) {
+        return nums;
+    }
+    const interNums = nums.reduce((acc, row) => {
+        row.forEach(num => acc.push(num));
+        return acc;
+    }, []);
+    if (r === 1) {
+        return [interNums];
+    }
+    for (let i = 0; i < interNums.length; i += c) {
+        let newRow = [];
+        for (let j = 0; j < c; j += 1) {
+            newRow.push(interNums[i + j]);
+        }
+        result.push(newRow);
+        newRow = [];
+    }
+    return result;
+};
+
+/*
+[ [ 1, 2 ],
+ [ 3, 4 ] ]
+*/
+console.log(matrixReshape(
+    [[1, 2],
+    [3, 4]], 2, 2,
+));
+
+/*
+[ [ 1, 2, 3, 4, 5 ],
+ [ 6, 7, 8, 9, 0 ] ]
+*/
+console.log(matrixReshape(
+    [[1, 2],
+    [3, 4],
+    [5, 6],
+    [7, 8],
+    [9, 0]],
+     2, 5,
+));
+
+export default matrixReshape;
