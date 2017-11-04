3f5134aa981e99155f593af39e28ad2fc2bb454c
Yossi Adler <joisadler@gmail.com>
2017-11-04 13:20:19 +0200

    add next greater element 1 accepted solution

--- 496_next-greater-element-1/next-greater-element-1.js
+++ 496_next-greater-element-1/next-greater-element-1.js
@@ -1,5 +1,15 @@
 const nextGreaterElement = (findNums, nums) => {
-    
+  const result = [];
+  findNums.forEach((number) => {
+    const [head, ...rest] = nums.slice(nums.indexOf(number));
+    const nge = rest.filter(element => element > head)[0] ?
+      rest.filter(element => element > head)[0] : -1;
+    result.push(nge);
+  });
+  return result;
 };
+
+console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2])); // [-1,3,-1]
+console.log(nextGreaterElement([2, 1, 3], [2, 3, 1])); // [3,-1,-1]
 
 export default nextGreaterElement;
