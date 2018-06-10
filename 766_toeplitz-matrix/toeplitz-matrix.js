63e645c4e7c921974e7951c89791435cf0736aa7
Yossi Adler <joisadler@gmail.com>
2018-06-10 20:41:08 +0300

    add "toeplitz matrix" accepted solution

--- 766_toeplitz-matrix/toeplitz-matrix.js
+++ 766_toeplitz-matrix/toeplitz-matrix.js
@@ -1,7 +1,11 @@
-/**
- * @param {number[][]} matrix
- * @return {boolean}
- */
 const isToeplitzMatrix = (matrix) => {
-
+  for (let i = 0; i < matrix.length - 1; i += 1) {
+    const row = matrix[i];
+    for (let j = 0; j < row.length - 1; j += 1) {
+      if (matrix[i + 1][j + 1] !== matrix[i][j]) {
+        return false;
+      }
+    }
+  }
+  return true;
 };
