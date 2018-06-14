e5c75d7cbaf7c69bfc6f7c4df6fa443e0b516e1c
Yossi Adler <joisadler@gmail.com>
2018-06-15 00:41:32 +0300

    add "maximum depth of binary tree" accepted solution

---
+++ 104_maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+// BFS solution based on the solution of the "Average of Levels in Binary Tree" problem
+const maxDepth = (node) => {
+  if (!node) {
+    return 0;
+  }
+  const queue = [];
+  const queueStates = [];
+  queue.push(node);
+  while (queue.length > 0) {
+    queueStates.push(queue.map(n => n.val));
+    const currentNode = queue.shift();
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+  }
+  let previousPassedElement = [queueStates[0]];
+  let previousPassedElementIndex = 0;
+  const levels = queueStates.filter((s, i, arr) => {
+    if (i === 0) return true;
+    if (i === previousPassedElement.length + previousPassedElementIndex) {
+      previousPassedElement = arr[i];
+      previousPassedElementIndex = i;
+      return true;
+    }
+    return false;
+  });
+  return levels.length;
+};
+
+export default maxDepth;
