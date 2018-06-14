1755cc8aec841690be3282636f2b4db645423097
Yossi Adler <joisadler@gmail.com>
2018-06-14 20:20:17 +0300

    add "average of levels in binary tree" accepted solution

--- 637_average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.js
+++ 637_average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.js
@@ -5,8 +5,34 @@
  *     this.left = this.right = null;
  * }
  */
-const averageOfLevels = (root) => {
-  
+
+const averageOfLevels = (node) => {
+  const queue = [];
+  const queueStates = [];
+  queue.push(node);
+  while (queue.length > 0) {
+    queueStates.push(queue.map(n => n.val));
+    const currentNode = queue.shift();
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+  }
+  let previousPassedElement = [queueStates[0]];
+  let previousPassedElementIndex = 0;
+  const levels = queueStates.filter((s, i, arr) => {
+    if (i === 0) return true;
+    if (i === previousPassedElement.length + previousPassedElementIndex) {
+      previousPassedElement = arr[i];
+      previousPassedElementIndex = i;
+      return true;
+    }
+    return false;
+  });
+  return levels.map(l => l.reduce((a, b) => a + b) / l.length);
 };
+
 
 export default averageOfLevels;
