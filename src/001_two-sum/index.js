cb0e5fdb040dfd0e56a1355e41440f1f7fc656f1
Yossi Adler <joisadler@gmail.com>
2023-07-05 19:13:09 +0300

    change variable name

--- src/001_two-sum/index.js
+++ src/001_two-sum/index.js
@@ -9,11 +9,11 @@ const twoSum = function (nums, target) {
   const hash = {};
   for (let i = 0; i < nums.length; i += 1) {
     const current = nums[i];
-    const diff = target - current;
+    const complement = target - current;
     if (hash[current] !== undefined) {
       return [hash[current], i];
     }
-    hash[diff] = i;
+    hash[complement] = i;
   }
 };
 
